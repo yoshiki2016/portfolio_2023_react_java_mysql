bc392e2287990c18e9376aa740827fa82949230a
Yoshiki Shin'ya <785431ys@gmail.com>
2023-06-27 10:15:25 +0900

    .gitignoreファイルを変更

---
+++ src/test/java/com/example/portfolio/java/mysql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio.java.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
