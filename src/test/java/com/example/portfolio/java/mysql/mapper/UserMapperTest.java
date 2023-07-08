1e473c62f3b5df1ce306369c7593cb7a65522b7d
Yoshiki Shin'ya <785431ys@gmail.com>
2023-07-09 07:56:15 +0900

    以下の記事をみて、MySQLの外部キー制約を無効化、有効化するSQLファイルを作成 UserMapperTestにてテストケースが実行出来る条件を整えた。 正常系1件作成済み https://blog.katsubemakito.net/mysql/mysql-foreignkey-disable

--- src/test/java/com/example/portfolio/java/mysql/mapper/UserMapperTest.java
+++ src/test/java/com/example/portfolio/java/mysql/mapper/UserMapperTest.java
@@ -3,13 +3,20 @@ package com.example.portfolio.java.mysql.mapper;
 import com.example.portfolio.java.mysql.entity.User;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DBRider
@@ -19,11 +26,30 @@ public class UserMapperTest {
     @Autowired
     UserMapper userMapper;
 
+    // テストケースの前処理、外部キー制約を無効にする。
+    @BeforeAll
+    public static void initializeDb(@Autowired DataSource dataSource){
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("user/foreignKeyChecksFalse.sql"));
+        populator.execute(dataSource);
+    }
+
+    // テストケースの後処理、外部キー制約を有効にする。
+    @AfterAll
+    public static void finalizeDb(@Autowired DataSource dataSource){
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("user/foreignKeyChecksTrue.sql"));
+        populator.execute(dataSource);
+    }
+
     @Test
-    @DataSet(value = "./user/userList.yml")
+    @DataSet(value = "user/userList.yml")
     @Transactional
     public void userテーブルに存在するusernameとpasswordのペアが存在する時Userを返却すること () {
         Optional<User> user = userMapper.login("k_kawai", "W-DSVjma");
         // assertThat を利用できるようにする。
+        assertThat(user).contains(
+                new User(2, "港一", "河合", "k_kawai", "W-DSVjma", "kouichikawai@ifkmjxhuh.kt")
+        );
     }
 }
