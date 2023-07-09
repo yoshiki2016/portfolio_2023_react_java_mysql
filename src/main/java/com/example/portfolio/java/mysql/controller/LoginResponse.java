82a33db72567d1c12a0b3db5f4ba8fddfe783dc5
Yoshiki Shin'ya <785431ys@gmail.com>
2023-07-09 15:15:10 +0900

    equals, hashCodeメソッドのオーバーライド

--- src/main/java/com/example/portfolio/java/mysql/controller/LoginResponse.java
+++ src/main/java/com/example/portfolio/java/mysql/controller/LoginResponse.java
@@ -1,5 +1,7 @@
 package com.example.portfolio.java.mysql.controller;
 
+import java.util.Objects;
+
 public class LoginResponse {
     private Integer userId;
     private Boolean loginFlag;
@@ -7,6 +9,19 @@ public class LoginResponse {
     public LoginResponse(Integer userId, Boolean loginFlag) {
         this.userId = userId;
         this.loginFlag = loginFlag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginResponse that = (LoginResponse) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(loginFlag, that.loginFlag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, loginFlag);
     }
 
     public Integer getUserId() {
