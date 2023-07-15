33d0c1cf70267035b5902df24be24097e2259682
Yoshiki Shin'ya <785431ys@gmail.com>
2023-07-15 20:23:50 +0900

    ユーザー更新：バックエンドのユニットテストと結合テスト作成

--- src/test/java/com/example/portfolio/java/mysql/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/example/portfolio/java/mysql/integrationtest/UserRestApiIntegrationTest.java
@@ -2,6 +2,7 @@ package com.example.portfolio.java.mysql.integrationtest;
 
 import com.example.portfolio.java.mysql.form.LoginForm;
 import com.example.portfolio.java.mysql.form.UserForm;
+import com.example.portfolio.java.mysql.form.UserUpdateForm;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
@@ -10,6 +11,8 @@ import org.codehaus.jackson.map.ObjectWriter;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.text.MatchesPattern.matchesPattern;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +32,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.sql.DataSource;
 
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -88,7 +94,7 @@ public class UserRestApiIntegrationTest {
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         JSONAssert.assertEquals("""
                 {
-                    "userId": null,
+                    "userId": 0,
                     "loginFlag": false
                 }
                 """, response, JSONCompareMode.STRICT);
@@ -123,4 +129,69 @@ public class UserRestApiIntegrationTest {
                 """, response, JSONCompareMode.STRICT);
     }
 
+    @Test
+    @DataSet(value = "user/userList.yml")
+    @ExpectedDataSet(value = "user/userUpdateList.yml")
+    @Transactional
+    void ユーザーをパスワードありとなしで更新出来ること() throws Exception {
+        // パスワードを除いて更新出来ること
+        UserUpdateForm userUpdateForm1 = new UserUpdateForm(1,"琴美2", "田中", "t_kotomi2",
+                false, "", "", "abc@example.com");
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String requestBody1 = ow.writeValueAsString(userUpdateForm1);
+        String response1 = mockMvc.perform(patch("/user_setting")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody1))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                {
+                    "message" : "the user successfully updated"
+                }
+                """, response1, JSONCompareMode.STRICT);
+
+        // パスワードを含んで更新が出来ること
+        UserUpdateForm userUpdateForm2 = new UserUpdateForm(2,"港一3", "河合", "k_kawai3",
+                true, "W-DSVjma", "P@ssword1!", "def@example.com");
+        String requestBody2 = ow.writeValueAsString(userUpdateForm2);
+        String response2 = mockMvc.perform(patch("/user_setting")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody2))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                {
+                    "message" : "the user successfully updated"
+                }
+                """, response2, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "user/userList.yml")
+    @Transactional
+    void 存在しないUserのIDとパスワードのペアを検知したらExceptionすること() throws Exception {
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(2023, 7, 15, 20, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)){
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
+
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            UserUpdateForm userUpdateForm = new UserUpdateForm(2,"港一3", "河合", "k_kawai3",
+                    true, "AAAAAAAA", "P@ssword1!", "def@example.com");
+            String requestBody = ow.writeValueAsString(userUpdateForm);
+            String response = mockMvc.perform(patch("/user_setting")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody))
+                    .andExpect(status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            JSONAssert.assertEquals("""
+               {
+                  "timestamp": "2023-07-15T20:00+09:00[Asia/Tokyo]",
+                  "message": "resource not found",
+                  "status": "404",
+                  "path": "/user_setting",
+                  "error": "Not Found"
+               }
+                """, response, JSONCompareMode.STRICT);
+        }
+    }
 }
--- src/test/java/com/example/portfolio/java/mysql/service/UserServiceImplTest.java
+++ src/test/java/com/example/portfolio/java/mysql/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.portfolio.java.mysql.service;
 
 import com.example.portfolio.java.mysql.controller.LoginResponse;
 import com.example.portfolio.java.mysql.entity.User;
+import com.example.portfolio.java.mysql.exception.ResourceNotFoundException;
 import com.example.portfolio.java.mysql.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -43,5 +45,35 @@ public class UserServiceImplTest {
         userServiceImpl.userRegister("山内", "秀加", "y_hideka",
                 "nm_8BrTv", "hideka28544@epjtdsmc.vyt.edk");
         Mockito.verify(userMapper).userRegister(testUser);
+    }
+
+    @Test
+    public void パスワードを除いてユーザー更新出来ること(){
+        userServiceImpl.updateUser(1,"琴美2", "田中", "t_kotomi2",
+                false, "", "", "abc@example.com");
+        Mockito.verify(userMapper).updateUserWithoutPassword(1,"琴美2", "田中",
+                "t_kotomi2", "abc@example.com");
+    }
+
+    @Test
+    public void ユーザーIDとパスワードを確認し合致したらパスワードを含んでユーザー更新出来ること(){
+        User oldUser = new User(2,"港一", "河合", "k_kawai",
+                "Abc12345", "abc@example.com");
+        Mockito.doReturn(Optional.of(oldUser))
+                .when(userMapper).searchUser(2, "Abc12345");
+
+        userServiceImpl.updateUser(2,"港一3", "河合", "k_kawai3",
+                true, "Abc12345", "P@ssword1!", "def@example.com");
+        Mockito.verify(userMapper).updateUserWithPassword(2,"港一3", "河合",
+                "k_kawai3", "P@ssword1!", "def@example.com");
+    }
+
+    @Test
+    public void ユーザーIDとパスワードを確認し合致しなかったら例外をthrowすること(){
+        Mockito.doReturn(Optional.empty()).when(userMapper).searchUser(2, "Abc12345");
+        assertThatThrownBy(() -> userServiceImpl.updateUser(2,"港一3", "河合", "k_kawai3",
+                true, "Abc12345", "P@ssword1!", "def@example.com"))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("resource not found");
     }
 }
